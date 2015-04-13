5f0b14a7cdc395f08cc5109714c6e8dd28d8b891
ebobtron <erobclark@att.net>
2015-04-13 12:54:54 -0500

    the basic core files to show a window

---
+++ resource.h
@@ -0,0 +1,11 @@
+#ifndef __RESOURCE_H_INCLUDED__
+#define __RESOURCE_H_INCLUDED__
+
+#define IDI_MYICON      101
+#define ID_FILE_EXIT    4001
+#define IDR_MYMENU      102
+#define IDI_MYICON2      201
+#define ID_FILE_EXIT2    9001
+#define ID_STUFF_GO     9002
+
+#endif // __RESOURCE_H_INCLUDED__
