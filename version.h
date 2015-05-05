57087db68eea84f24e4ea37671b227e1bbc7bd4b
ebobtron <erobclark@att.net>
2015-05-05 16:12:44 -0500

    release cadidate 1 we have a playable game

--- version.h
+++ version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-	
+
 	//Date Version Types
-	static const char DATE[] = "28";
-	static const char MONTH[] = "04";
+	static const char DATE[] = "05";
+	static const char MONTH[] = "05";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.04";
-	
+	static const char UBUNTU_VERSION_STYLE[] =  "15.05";
+
 	//Software Status
-	static const char STATUS[] =  "Beta";
-	static const char STATUS_SHORT[] =  "b";
-	
+	static const char STATUS[] =  "Alpha";
+	static const char STATUS_SHORT[] =  "a";
+
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 3;
-	static const long REVISION  = 14;
-	
+	static const long BUILD  = 4;
+	static const long REVISION  = 24;
+
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 116;
-	#define RC_FILEVERSION 1,0,3,14
-	#define RC_FILEVERSION_STRING "1, 0, 3, 14\0"
-	static const char FULLVERSION_STRING [] = "1.0.3.14";
-	
+	static const long BUILDS_COUNT  = 134;
+	#define RC_FILEVERSION 1,0,4,24
+	#define RC_FILEVERSION_STRING "1, 0, 4, 24\0"
+	static const char FULLVERSION_STRING [] = "1.0.4.24";
+
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 3;
-	
+	static const long BUILD_HISTORY  = 4;
+
 
 }
 #endif //VERSION_H
